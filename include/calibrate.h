2854e4751ef7e1f461f63c70498a6df47306c87f
Dante-666 <j.singh.logan@gmail.com>
2017-03-14 23:10:54 +0530

    dummy

--- include/calibrate.h
+++ include/calibrate.h
@@ -4,16 +4,23 @@
 
 class Calibrate {
 public:
-  Calibrate();
+  Calibrate(float calibSquareDimension, cv::Size chessboardDimension);
   void getIntrinsics();
   void getDistortionCoeffs();
+  void getChessboardCorners(cv::Mat image, bool is_hd);
 private:
-  std::vector<std::vector<int> > p_hd;
+  const float calibSquareDimension;
+  const cv::Size chessboardDimension;
+  std::vector<std::vector<cv::Point2f> > marker_corners_hd;
+  std::vector<std::vector<cv::Point2f> > marker_corners_ir;
+
+
+  /*std::vector<std::vector<int> > p_hd;
   std::vector<std::vector<int> > p_ir;
   Eigen::Matrix3d intr_hd;
   Eigen::Matrix3d intr_ir;
   std::vector<float> dist_coeff_hd;
   std::vector<float> dist_coeff_ir;
   Eigen::Matrix3d R;
-  Eigen::Vector3d t;
+  Eigen::Vector3d t;*/
 };
--- src/calibrate.cc
+++ src/calibrate.cc
@@ -1,5 +1,45 @@
 #include "calibrate.h"
 
+Calibrate::Calibrate(float calibSquareDimension, cv::Size chessboardDimension) {
+  this.calibSquareDimension = calibSquareDimension;
+  this.chessboardDimension = chessboardDimension;
+}
+
 void Calibrate::getIntrinsics() {
-  
+
+}
+
+int Calibrate::getChessboardCorners(cv::Mat image, bool is_hd) {
+  std::vector<cv::Point2f> pointBuf;
+  bool found = cv::findChessboardCorners(image, this.chessboardDimension, pointBuf, CV_CALIB_CB_ADAPTIVE_THRESHOLD | CV_CALIB_CB_NORMALIZE_IMAGE);
+  if (found) {
+    if (is_hd) this.marker_corners_hd.push_back(pointBuf);
+    else this.marker_corners_ir.push_back(pointBuf);
+  }
+  cv:Mat draw_image;
+  image.copyTo(draw_image);
+
+  cv::drawChessboardCorners(draw_image, this.chessboardDimension, pointBuf, found);
+  cv::imshow("Checkerboard", draw_image);
+  cv::waitKey(1);
+
+  if (is_hd) return this.marker_corners_hd.size();
+  else return this.marker_corners_ir.size();
+}
+
+int main(int argc, char** argv) {
+
+  /**
+   * Take images first from the HD camera and get the parameters
+   */
+
+  cv::Size chessboardDimension = Size(argv[2], argv[3]);
+
+  Calibrate calib = new Calibrate(argv(1)), chessboardDimension);
+
+  while(true) {
+    cv::Mat image = cv::Zeros(100, 100); //Read from the camera
+    calib.getChessboardCorners(image, true); // appends the vector
+  }
+
 }
